2b7e4755be12e572b5241ff1a9e185d0186d077b
Gabriel Wallace <gabriel.wallace.dev@gmail.com>
2018-04-20 00:45:42 -0500

    Continue

--- ics372_thatgroup_android/ClinicalTrialClient/app/src/main/java/edu/metrostate/ics372/thatgroup/clinicaltrial/android/ImportActivity.java
+++
@@ -1,5 +0,0 @@
-package edu.metrostate.ics372.thatgroup.clinicaltrial.android;
-
-public class ImportActivity  {
-
-}
--- ics372_thatgroup_android/ClinicalTrialClient/app/src/main/java/edu/metrostate/ics372/thatgroup/clinicaltrial/android/readingsactivity/presenter/ReadingsPresenter.java
+++
@@ -1,4 +0,0 @@
-package edu.metrostate.ics372.thatgroup.clinicaltrial.android.readingsactivity.presenter;
-
-public class ReadingsPresenter {
-}
