4c99091b9dce31d6f403c97d0b5fa4423a26335e
Vincent J. Palodichuk <vj.palodichuk@gmail.com>
2018-04-21 21:00:53 -0500

    Working on Clinics
    Signed-off-by: Vincent J. Palodichuk <vj.palodichuk@gmail.com>

--- ics372_thatgroup_android/ClinicalTrialClient/app/src/main/java/edu/metrostate/ics372/thatgroup/clinicaltrial/android/NewClinicView.java
+++ ics372_thatgroup_android/ClinicalTrialClient/app/src/main/java/edu/metrostate/ics372/thatgroup/clinicaltrial/android/NewClinicView.java
@@ -2,5 +2,7 @@ package edu.metrostate.ics372.thatgroup.clinicaltrial.android;
 
 public interface NewClinicView extends BaseView<NewClinicPresenter>{
 
+    void setId(String id);
 
+    void setName(String name);
 }
